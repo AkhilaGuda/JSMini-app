1bbb7cbaa97737cd8545fdbafa9ef15f4c2b3e9c
akhilaGuda <akhila.guda@beautifulcode.in>
2025-08-12 10:07:39 +0530

    updated js file

--- index.js
+++ index.js
@@ -2,50 +2,64 @@ const userDropdown = document.getElementById('user-dropdown');
 const albumList = document.getElementById('album-list');
 const photoGallery = document.getElementById('photo-Gallery');
 
-function createOption(value, text) {
+const createOption = (value, text) => {
     const option = document.createElement('option');
     option.value = value;
     option.textContent = text;
     return option;
 }
 
-function createAlbumItem(album) {
+const createAlbumItem = (album, albums) => {
     const button = document.createElement('button');
     button.textContent = album.title;
-    button.addEventListener('click', () => loadPhotos(album.id));
+    button.addEventListener('click', () => loadPhotos(album.id, albums));
     return button;
 }
 
-async function loadUsers() {
+const loadUsers = async () => {
     try {
         const res = await fetch('https://jsonplaceholder.typicode.com/users');
-        users = await res.json();
+        const users = await res.json();
         users.forEach(user => {
             userDropdown.appendChild(createOption(user.id, user.username));
         });
+        userDropdown.addEventListener('change', () => {
+            const userId = userDropdown.value;
+            if (userId) {
+                loadAlbums(userId);
+                const heading2 = document.getElementById('heading2');
+                let resultingObject = users.find(users => users.id == userId);
+                heading2.innerHTML = `Albums by ${resultingObject.username}`;
+                const heading3 = document.getElementById('heading3');
+                heading3.innerHTML = '';
+            }
+
+
+        });
+
     }
     catch (err) {
         console.error('failed to load users', err);
     }
 }
 
-async function loadAlbums(userId) {
+const loadAlbums = async (userId) => {
     albumList.innerHTML = '';
     photoGallery.innerHTML = '';
     try {
         const res = await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
-         albums = await res.json();
+        let albums = await res.json();
         albums.forEach(album => {
-            albumList.appendChild(createAlbumItem(album));
-            albumList.className="albums";
-            
+            albumList.appendChild(createAlbumItem(album, albums));
+            albumList.className = "albums";
+
         })
     }
     catch (err) {
         console.error('unable to load albums ', err);
     }
 }
-async function loadPhotos(albumId) {
+const loadPhotos = async (albumId, albums) => {
     photoGallery.innerHTML = '';
     try {
         const res = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`);
@@ -54,29 +68,18 @@ async function loadPhotos(albumId) {
             const img = document.createElement('div');
             img.src = photo.thumbnailUrl;
             img.textContent = photo.title;
-            img.className="image";
-            img.style.padding='20px';
+            img.className = "image";
+            img.style.padding = '20px';
             photoGallery.appendChild(img);
         });
-        const heading3= document.getElementById('heading3');
-        let resultingObject=albums.find(albums=> albums.id==albumId);
-        heading3.innerHTML=`Albums by ${resultingObject.title}`;
+        const heading3 = document.getElementById('heading3');
+        let resultingObject = albums.find(albums => albums.id == albumId);
+        heading3.innerHTML = `Album photos: ${resultingObject.title}`;
     }
     catch (err) {
         console.error('failed to load photos ', err);
     }
 }
-
-userDropdown.addEventListener('change', () => {
-    const userId = userDropdown.value;
-    if (userId){loadAlbums(userId);
-        const heading2= document.getElementById('heading2');
-        let resultingObject=users.find(users=> users.id==userId);
-        heading2.innerHTML=`Albums by ${resultingObject.username}`;
-    }
-
-
-});
 
 loadUsers();
 
